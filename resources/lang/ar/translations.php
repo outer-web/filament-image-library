c45b981c2f1119793d76789a86a0e676134b0980
Karam <karam.nassar@hotmail.com>
2024-04-06 20:32:42 +0300

    Added Arabic Translations

---
+++ resources/lang/ar/translations.php
@@ -0,0 +1,102 @@
+<?php
+
+return [
+    'actions' => [
+        'apply' => 'تطبيق',
+        'deselect' => 'إلغاء الاختيار',
+        'close_upload_action' => 'إغلاق',
+        'crop' => 'اقتصاص',
+        'delete' => 'حذف',
+        'edit' => 'تعديل المعلومات',
+        'refresh' => 'تحديث',
+        'save' => 'حفظ',
+        'select' => 'اختيار',
+        'select_existing' => 'اختيار من الموجود',
+        'show_selected_only' => 'إظهار المختارة فقط',
+        'upload' => 'رفع',
+        'reorder' => 'اسحب لإعادة الترتيب',
+    ],
+    'conversions' => [
+        'labels' => [
+            'thumbnail' => 'صورة مصغرة',
+        ],
+    ],
+    'states' => [
+        'selected' => 'مختارة',
+    ],
+    'pagination' => [
+        'next' => 'التالي',
+        'previous' => 'السابق',
+    ],
+    'disks' => [
+        'public' => 'عام',
+    ],
+    'fields' => [
+        'picker' => [
+            'label' => 'صورة',
+        ],
+    ],
+    'form' => [
+        'labels' => [
+            'alt' => 'نص بديل',
+            'disk' => 'الموقع',
+            'title' => 'عنوان',
+            'upload' => 'اختيار ملف',
+            'upload_multiple' => 'اختيار ملف واحد أو أكثر',
+            'image_picker' => [
+                'upload_only' => 'رفع صورة',
+                'upload_or_select' => 'رفع أو اختيار صورة',
+                'multiple' => [
+                    'upload_only' => 'رفع صورة (صور)',
+                    'upload_or_select' => 'رفع أو اختيار صورة (صور)',
+                ],
+            ],
+        ],
+        'help' => [
+            'alt' => 'يتم استخدام النص البديل لقارئات الشاشة ومحركات البحث.',
+            'title' => 'يظهر العنوان عند تحريك المؤشر فوق الصورة.',
+        ],
+        'messages' => [
+            'no_image_selected' => 'لم يتم اختيار أي صورة.',
+            'no_images_selected' => 'لم يتم اختيار أي صور.',
+            'upload_or_select_existing' => 'رفع أو اختيار صورة.',
+            'upload_or_select_existing_multiple' => 'رفع أو اختيار صورة واحدة أو أكثر.',
+        ],
+        'steps' => [
+            'labels' => [
+                'information' => 'معلومات',
+                'upload' => 'رفع',
+            ],
+        ]
+    ],
+    'modals' => [
+        'delete' => [
+            'description' => 'هل أنت متأكد من أنك تريد حذف هذه الصورة؟',
+            'heading' => 'حذف صورة',
+            'submit' => 'حذف',
+        ],
+    ],
+    'notifications' => [
+        'image_deleted' => [
+            'title' => 'تم حذف الصورة',
+        ],
+        'image_updated' => [
+            'title' => 'تم تحديث الصورة',
+        ],
+        'image_cropped' => [
+            'title' => 'تم اقتصاص الصورة',
+        ],
+        'image_uploaded' => [
+            'title' => 'تم رفع الصورة',
+        ],
+        'images_uploaded' => [
+            'title' => 'تم رفع الصور',
+        ],
+    ],
+    'no_images_found' => '',
+    'page' => [
+        'title' => 'مكتبة الصور',
+        'navigation_label' => 'مكتبة الصور',
+        'navigation_group' => '',
+    ],
+];
